8d43e702ca00662f592a8c1488af003ad9718168
Sharktheone <techn1ck@gmx.de>
2022-11-03 20:32:31 +0100

    Basic Plugin

---
+++ src/main/java/de/scharschbot/velocity/plugin/Plugin.kt
@@ -0,0 +1,112 @@
+package de.scharschbot.velocity.plugin
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.google.inject.Inject
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.plugin.Plugin
+import com.velocitypowered.api.event.lifecycle.ProxyInitializeEvent
+import com.velocitypowered.api.event.player.DisconnectEvent
+import com.velocitypowered.api.event.player.PostLoginEvent
+import com.velocitypowered.api.proxy.ProxyServer
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.auth.BasicScheme
+import org.apache.http.impl.client.HttpClients
+import org.slf4j.Logger
+import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+@Plugin(
+    id = "scharschbot",
+    name = "ScharschBotVelocity",
+    version = "0.1.0",
+    authors = ["SharkTheOne"]
+
+)
+class Plugin {
+    private var server: ProxyServer? = null
+    private var logger: Logger? = null
+    private lateinit var config: JsonNode
+
+    @Inject fun scharschBot(plugin: Plugin, server: ProxyServer, logger: Logger){
+        this.server = server
+        this.logger = logger
+        this.config = getConfig()
+        logger.info("ScharschBot Velocity Plugin Loaded!")
+
+    }
+
+
+    private fun getConfig(): JsonNode {
+            val configName = "config.yml"
+            val configPath = Paths.get("./plugins/ScharschBot/$configName")
+            val config = File(configPath.toString())
+            if(!config.exists()){
+                try {
+                    Files.createDirectories(Paths.get(config.parent))
+                } catch (e: IOException) {
+                    // ignore
+                }
+                try {
+                    javaClass.classLoader.getResourceAsStream(configName).use { standardConfig ->
+                        if (standardConfig != null) {
+                            Files.copy(
+                                standardConfig,
+                                configPath
+                            )
+                        }
+                    }
+                } catch (e: IOException) {
+                    throw RuntimeException(e)
+                }
+            }
+            val mapper = ObjectMapper(YAMLFactory())
+
+
+            return mapper.readTree(config)
+        }
+
+    private fun sendValues(Data: String){
+        val httpClient = HttpClients.createDefault()
+        try {
+            val request = HttpPost(config.get("URL")?.asText())
+            val creds = UsernamePasswordCredentials(config.get("User")?.asText(),config.get("Pass")?.asText())
+
+            request.entity = StringEntity(Data)
+            request.setHeader("Content-type", "application/json")
+            request.addHeader(BasicScheme().authenticate(creds, request, null))
+
+            val response: CloseableHttpResponse = httpClient.execute(request)
+            if ( !(response.statusLine.statusCode == 204 || response.statusLine.statusCode == 200) ) {
+                logger?.warn("Failure sending data to discord bot: " + response.statusLine.reasonPhrase)
+            }
+            response.close()
+            httpClient.close()
+        } catch (e: Exception) {
+            logger?.warn("Failed to send HTTP Request: " + e.message)
+        }
+    }
+
+    @Subscribe
+    fun onProxyInitialization(event: ProxyInitializeEvent?) {
+        server?.eventManager()?.register(this, this)
+    }
+
+    @Subscribe
+    fun playerJoin(event: PostLoginEvent){
+        val joinJson = "{\"name\":\"" + event.player().toString() + "\", \"type\":\"join\", \"server\":\"" + config.get("ServerName")?.asText() + "\"}"
+        sendValues(joinJson)
+    }
+
+    @Subscribe
+    fun playerQuit(event: DisconnectEvent){
+        val quitJson = "{\"name\":\"" + event.player().toString() + "\", \"type\":\"quit\", \"server\":\"" + config.get("ServerName")?.asText() + "\"}"
+        sendValues(quitJson)
+    }
+}
