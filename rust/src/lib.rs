97abf7b7bc6a728a5eff7c5bf2a3bf0e00478be3
Sharktheone <sharktheone@proton.me>
2023-04-15 23:49:19 +0200

    add TODOs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,3 +11,37 @@ pub unsafe extern "C" fn Java_de_scharschbot_velocity_plugin_Plugin_test(mut env
 
     println!("Hello, {}!", name);
 }
+
+// TODO: Config Loader
+
+// TODO: PlayerJoinEvent
+// TODO: PlayerQuitEvent
+// TODO: PlayerChatEvent
+
+// TODO: WS EVENTS:
+
+
+// TODO: AUTH: auth
+
+// TODO: SEND: sendPlayers
+// TODO: SEND: playerJoined
+// TODO: SEND: playerLeft
+// TODO: SEND: chatMessage
+// TODO: SEND: reportPlayer
+// TODO: SEND: banPlayer
+// TODO: SEND: unbanPlayer
+// TODO: SEND: kickPlayer
+// (TODO: SEND: playerAdvancement)
+// (TODO: SEND: playerDeath)
+
+
+// TODO: RECEIVE: sendChatMessage
+// TODO: RECEIVE: sendPlayers
+// TODO: RECEIVE: kickPlayer
+// TODO: RECEIVE: banPlayer
+// TODO: RECEIVE: unbanPlayer
+// TODO: RECEIVE: sendCommand
+
+
+
+
