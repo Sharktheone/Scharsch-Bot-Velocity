1f4278c2105ebbdca6d68c74c4c3bc31bd31cd41
Sharktheone <sharktheone@proton.me>
2023-04-16 21:45:15 +0200

    add Event class

---
+++ src/main/java/de/scharschbot/velocity/plugin/Events.kt
@@ -0,0 +1,43 @@
+package de.scharschbot.velocity.plugin
+
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.connection.DisconnectEvent
+import com.velocitypowered.api.event.connection.PostLoginEvent
+import java.io.File
+import java.nio.file.Files
+
+
+class Events constructor(private val logger: org.slf4j.Logger) {
+    init {
+        val libName = "libscharsch_bot_velocity"
+        var libExtension = ".so"
+
+        val osName = System.getProperty("os.name")
+
+        if (osName.contains("Windows")) {
+            libExtension = ".dll";
+        } else if (osName.contains("Mac")) {
+            libExtension = ".dylib";
+        }
+        val libDir = Files.createTempDirectory("ScharschBot")
+        libDir.toFile().deleteOnExit()
+        val libFile = File(libDir.toFile(), libName)
+
+
+        javaClass.classLoader.getResourceAsStream(libName + libExtension).use { input ->
+            if (input == null) {
+                throw RuntimeException("Could not find ScharschBot library $libName")
+            }
+            Files.copy(input, libFile.toPath())
+        }
+
+        System.load(libFile.absolutePath)
+        logger.info("ScharschBot Velocity Plugin Loaded!")
+    }
+
+    @Subscribe
+    external fun onPlayerJoin(event: PostLoginEvent)
+
+    @Subscribe
+    external fun onPlayerLeave(event: DisconnectEvent)
+}
