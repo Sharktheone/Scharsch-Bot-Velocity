064d7345e5f515176ba990cb1bf32a852033bfd3
Sharktheone <sharktheone@proton.me>
2023-04-17 22:43:01 +0200

    player Left event

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -12,7 +12,7 @@ pub unsafe extern "C" fn Java_de_scharschbot_velocity_plugin_Plugin_test(mut env
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn Java_de_scharschbot_velocity_plugin_Plugin_onPlayerJoin(mut env: JNIEnv, class: JClass, event: JClass) {
+pub unsafe extern "C" fn Java_de_scharschbot_velocity_plugin_Plugin_onPlayerJoin(mut env: JNIEnv, _class: JClass, event: JClass) {
     println!("A player Joined!");
     let player_obj = match env.call_method(event, "getPlayer", "()Lcom/velocitypowered/api/proxy/Player;", &[]) {
         Ok(obj) => obj.l().unwrap(),
@@ -40,6 +40,38 @@ pub unsafe extern "C" fn Java_de_scharschbot_velocity_plugin_Plugin_onPlayerJoin
 
     println!("Player Joined: {}!", name);
 }
+
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_de_scharschbot_velocity_plugin_Plugin_onPlayerLeave(mut env: JNIEnv, _class: JClass, event: JClass) {
+    println!("A player Joined!");
+    let player_obj = match env.call_method(event, "getPlayer", "()Lcom/velocitypowered/api/proxy/Player;", &[]) {
+        Ok(obj) => obj.l().unwrap(),
+        Err(e) => {
+            eprintln!("Error getting player object: {}", e);
+            return;
+        }
+    };
+
+    let player_name = match env.call_method(player_obj, "getUsername", "()Ljava/lang/String;", &[]) {
+        Ok(name) => name.l().unwrap(),
+        Err(e) => {
+            eprintln!("Error getting player name: {}", e);
+            return;
+        }
+    };
+
+    let name: String = match env.get_string(JString::from(player_name).as_ref()) {
+        Ok(s) => s.into(),
+        Err(e) => {
+            eprintln!("Error getting string: {}", e);
+            return;
+        }
+    };
+
+    println!("Player Left: {}!", name);
+}
+
 
 // TODO: Config Loader
 
